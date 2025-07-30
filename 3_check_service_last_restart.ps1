44c70362b8adea276e7579ddc857a20647bc356a
Daniel Gómez <57914973+DanielGomez98@users.noreply.github.com>
2025-07-30 08:41:00 -0600

    Update 3_check_service_last_restart.ps1

--- 3_check_service_last_restart.ps1
+++ 3_check_service_last_restart.ps1
@@ -1,1 +1,6 @@
 # Verifica última vez iniciado...
+$ServiceName = "Spooler"
+$svc = Get-WmiObject -Class Win32_Service -Filter "Name='$ServiceName'"
+$LastStateChange = $svc.ConvertToDateTime($svc.Started)
+
+Write-Output "El servicio '$ServiceName' fue iniciado por última vez en: $LastStateChange"
